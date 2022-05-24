809be21a6f6f46e7b3e11d47e91016e2329298a3
pankajishwarkar1995 <105146931+pankajishwarkar1995@users.noreply.github.com>
2022-05-24 14:41:16 +0530

    Add files via upload

---
+++ assignment.1.py
@@ -0,0 +1,14 @@
+# st = [1,2,3,4,5,6,7,8,9]
+# even = 0
+# odd = 0
+# for i in list :
+#     if (i%2==0) :
+#         even=even+1
+#     else :
+#         odd=odd+1
+# print("even number in list")
+# print("odd number in list")
+
+topping1 = "Peanut Butter"
+topping2 = "Jelly"
+sandwich = topping1 + "&" + topping2
